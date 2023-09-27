0a2867b0526b9174a0e80b8d0873e9116b5d6f0b
linuxhpceng <58539716+linuxhpceng@users.noreply.github.com>
2023-09-27 16:32:38 -0500

    trying VPC_ID

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
   name = "blog_new"
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.vpc.vpc_id
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
