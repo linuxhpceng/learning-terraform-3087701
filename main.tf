75c6b0e4d857f60c0c0ae665e2f7d292c4b4d5ac
linuxhpceng <58539716+linuxhpceng@users.noreply.github.com>
2023-09-27 14:24:04 -0500

    correction 2

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "aws_security_group_rule" "blog_http_in" {
 
 }
 
-resource "aws_security_group_rule "blog_https_in" {
+resource "aws_security_group_rule" "blog_https_in" {
   type = "ingress"
   from_port   = 443
   to_port     = 443
@@ -56,7 +56,7 @@ resource "aws_security_group_rule "blog_https_in" {
 
 }
 
-resource "aws_security_group_rule "blog_everything_out" {
+resource "aws_security_group_rule" "blog_everything_out" {
   type = "ingress"
   from_port   = 0
   to_port     = 0
