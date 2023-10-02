ed37f69e7e75bab1762d2fe25bae7afbf4875e24
linuxhpceng <58539716+linuxhpceng@users.noreply.github.com>
2023-10-02 13:28:55 -0500

    remove isntance

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
-}
+#data "aws_ami" "app_ami" {
+#  most_recent = true
+#
+#  filter {
+#    name   = "name"
+#    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+#  }
+#
+#  filter {
+#    name   = "virtualization-type"
+#    values = ["hvm"]
+#  }
+#
+#  owners = ["979382823631"] # Bitnami
+#}
 
 #module "vpc" {
 #  source = "terraform-aws-modules/vpc/aws"
